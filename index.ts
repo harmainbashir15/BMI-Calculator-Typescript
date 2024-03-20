40da3bdc0fa8e89c1f888c52a139254bddcac7c0
Harmain Bashir <162583900+harmainbashir15@users.noreply.github.com>
2024-03-20 10:50:30 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,9 @@
+let weightInkg =  52;
+let heightInmeters = 1.6;               
+
+//BMI formula = weight in kg divided by height in meters * height in meters
+
+let bmi = weightInkg / (heightInmeters * heightInmeters)
+            //52             //1.6     *      //1.6
+
+console.log(`your BMI is ${bmi}`);
